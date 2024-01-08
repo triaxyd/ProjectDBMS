f64acda4bfc4e37c02eb1be0252768c6d33474e8
Triantafyllos Xydis <triantafyllosxyd@gmail.com>
2024-01-08 21:26:53 +0200

    finished queries1

--- queries1.sql
+++ queries1.sql
@@ -1,32 +1,16 @@
 
 -- i. *
-select count(lon) as longitude,count(lat) as latitude , t::date
+select date(t) as day, count(*) as coordinate_count 
 from positions
-group by t::date
-order by longitude,latitude desc;
+group by day
+order by coordinate_count desc;
 
 
 -- ii. **
-with greek_vessels as
-(
-    select type
-    from vessels
-    where flag like '%Greece%'
-)
-
-with view_greek as
-(
-    select count(*) as number_of_greek, vessel_types.description, vessel_types.code
-    from vessel_types inner join greek_vessels on vessel_types.code = greek_vessels.type
-    group by vessel_types.code
-)
-
-/*
-select count(*) as number_of_greek, vessel_types.description
-from vessels inner join vessel_types on vessels.type = vessel_types.code
-where flag like 'Greece'
+select vessel_types.code,count(vessels.id) 
+from vessel_types left join vessels on vessel_types.code = vessels.type
+and vessels.flag = 'Greece'
 group by vessel_types.code;
-*/
 
 -- iii. ***
 with ships_speed_greater_30 as
@@ -37,43 +21,50 @@ with ships_speed_greater_30 as
 )
 
 select vessel_types.description as vessel_type,count(*) as ships_total 
-from ships_speed_greater_30 inner join vessels on ships_speed_greater_30.vessel_id = vessels.id
-    inner join vessel_types on vessels.type = vessel_types.code
+from ships_speed_greater_30 join vessels on ships_speed_greater_30.vessel_id = vessels.id
+    join vessel_types on vessels.type = vessel_types.code
 group by vessel_types.description;
 
 
 -- iv. ****
+/* 
+select date(positions.t) as day, count(*) as coordinate_count
+from positions
+join vessels on positions.vessel_id = vessels.id
+where vessels.type between 60 and 69
+and positions.t between '2019-08-14'::timestamp and '2019-08-19'::timestamp
+group by day;
+*/
+
 with passenger_vessels as
 (
     select vessels.id
-    from vessel_types inner join vessels on vessel_types.code = vessels.type
+    from vessel_types join vessels on vessel_types.code = vessels.type
     where vessel_types.description like 'Passenger%'
 )
 
 select count(*) as vessels_per_day
-from positions inner join passenger_vessels on positions.vessel_id = passenger_vessels.id
+from positions join passenger_vessels on positions.vessel_id = passenger_vessels.id
 group by positions.t::date 
 having positions.t::date >= '2019-08-14' and positions.t::date <= '2019-08-18'
-
-
 
 
 -- v. ******
 with cargo_vessels as
 (
     select vessels.id
-    from vessel_types inner join vessels on vessel_types.code = vessels.type
+    from vessel_types join vessels on vessel_types.code = vessels.type
     where vessel_types.description like 'Cargo%'
 )
 
 --some point in period 15/08/2019-18/08/2019
 select distinct(cargo_vessels.id) as cargo_static
-from cargo_vessels inner join positions on cargo_vessels.id = positions.vessel_id
+from cargo_vessels join positions on cargo_vessels.id = positions.vessel_id
 where positions.speed = 0 and (t::date between '2019-08-15' and '2019-08-18');  
 
 -- every day in the period 12/08/2019-19/08/2019
 select cargo_vessels.id
-from cargo_vessels inner join positions on cargo_vessels.id = positions.vessel_id
+from cargo_vessels join positions on cargo_vessels.id = positions.vessel_id
 where positions.speed = 0 and t>='2019-08-12' and t<='2019-08-19'
 group by cargo_vessels.id 
 having count(distinct date(t)) = (date '2019-08-19' - date '2019-08-12');
