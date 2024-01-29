cf0b85131e7ab5d29956b0fd80278fdac6a6c397
Thespiann <100039436+Thespiann@users.noreply.github.com>
2024-01-29 13:21:06 +0200

    change names, 3rd query

--- indexes.sql
+++ indexes.sql
@@ -1,12 +1,12 @@
---1st and 4th query
-create index coords_dates_index on positions((t::date),lon,lat);
+--1st query
+create index dates_coords_index on positions((t::date),lon,lat);
 --2nd query
 create index greek_flag_index on vessels(flag,id,type) where flag='Greece';
---3rd query, not used on max_parallel_workers_per_gather=8
-create index positions_speed_30_index on positions(speed) where speed>30;
-create index vessels_type_index on vessels(type);
+--3rd query
+create index vessel_id_speed_index on positions(vessel_id,speed);
+--create index vessels_type_index on vessels(type); --doesnt improve time
 --4th query
-create index id_coords_dates_index on positions(vessel_id,(t::date),lon,lat);
+create index vessel_id_dates_coords_index on positions(vessel_id,(t::date),lon,lat);
 --5th query
-create index id_speed_dates_index on positions(vessel_id,(t::date),speed) where speed=0;
+create index vessel_id_speed_dates_index on positions(vessel_id,(t::date),speed) where speed=0;
 
